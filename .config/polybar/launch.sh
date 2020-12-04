6a61d543eeb8774c8418b6931005dc62ceb0ebb5
CBreinholt <cmbreinholt@gmail.com>
2020-12-04 03:01:19 -0700

    temporaary changes before overhaul

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch main status bar
-polybar -r -c ~/.config/polybar/config.ini main &
+polybar -r -c ~/.config/polybar/config-onedark.ini main &
+#polybar -r -c ~/.config/polybar/config.ini main &
 #polybar -r -c ~/.config/polybar/config.ini dual &
